457dfc5ccd09755e77ecb31ef74073f81430b394
viking98070 <drewirvine@gmail.com>
2026-05-08 21:08:32 -0700

    Update StepTeams.js

--- src/components/StepTeams.js
+++ src/components/StepTeams.js
@@ -18,6 +18,7 @@ const TEAMS = {
 const LABELS = { nfl:'NFL', mlb:'MLB', nba:'NBA', nhl:'NHL', cfb:'College Football', mcbb:"Men's CBB", wcbb:"Women's CBB", mls:'MLS', wnba:'WNBA' }
 
 const TEAM_COLORS = {
+  // NFL
   Bears:'#0B162A',Bengals:'#FB4F14',Bills:'#00338D',Broncos:'#FB4F14',Browns:'#311D00',
   Buccaneers:'#D50A0A',Cardinals:'#97233F',Chargers:'#0080C6',Chiefs:'#E31837',Colts:'#002C5F',
   Cowboys:'#003594',Dolphins:'#008E97',Eagles:'#004C54',Falcons:'#A71930',Giants:'#0B2265',
@@ -25,24 +26,28 @@ const TEAM_COLORS = {
   Patriots:'#002244',Raiders:'#000000',Rams:'#003594',Ravens:'#241773',Saints:'#9B8A5A',
   Seahawks:'#002244',Steelers:'#101820',Texans:'#03202F',Titans:'#0C2340',Vikings:'#4F2683',
   '49ers':'#AA0000',Washington:'#5A1414',
+  // MLB
   Angels:'#BA0021',Astros:'#EB6E1F',Athletics:'#003831','Blue Jays':'#134A8E',Braves:'#CE1141',
   Brewers:'#FFC52F',Cubs:'#0E3386',Dodgers:'#005A9C',Guardians:'#E31937',Mariners:'#0C2C56',
   Mets:'#002D72',Nationals:'#AB0003',Orioles:'#DF4601',Padres:'#2F241D',Phillies:'#E81828',
   Pirates:'#27251F',Rangers:'#003278',Rays:'#092C5C','Red Sox':'#BD3039',Reds:'#C6011F',
   Rockies:'#33006F',Royals:'#004687',Tigers:'#0C2340',Twins:'#002B5C','White Sox':'#27251F',
   Yankees:'#003087',
+  // NBA
   '76ers':'#006BB6',Bucks:'#00471B',Bulls:'#CE1141',Cavaliers:'#860038',Celtics:'#007A33',
   Clippers:'#C8102E',Grizzlies:'#5D76A9',Hawks:'#E03A3E',Heat:'#98002E',Hornets:'#1D1160',
   Jazz:'#002B5C',Kings:'#5A2D81',Knicks:'#006BB6',Lakers:'#552583',Magic:'#0077C0',
   Mavericks:'#00538C',Nets:'#000000',Nuggets:'#0E2240',Pacers:'#002D62',Pelicans:'#0C2340',
   Pistons:'#C8102E',Raptors:'#CE1141',Rockets:'#CE1141',Spurs:'#C4CED4',Suns:'#1D1160',
   Thunder:'#007AC1',Timberwolves:'#0C2340','Trail Blazers':'#E03A3E',Warriors:'#1D428A',Wizards:'#002B5C',
+  // NHL
   Blackhawks:'#CF0A2C','Blue Jackets':'#002654',Blues:'#002F87',Bruins:'#FCB514',Canucks:'#00843D',
   Capitals:'#041E42',Devils:'#CE1126',Ducks:'#F47A38',Flames:'#C8102E',Flyers:'#F74902',
   'Golden Knights':'#B4975A',Hurricanes:'#CC0000',Islanders:'#00539B',Kings:'#111111',
   Kraken:'#001628',Lightning:'#002868','Maple Leafs':'#00205B',Oilers:'#FF4C00',
   Penguins:'#CFC493',Predators:'#FFB81C',Rangers:'#0038A8','Red Wings':'#CE1126',
   Sabres:'#003087',Senators:'#C8102E',Sharks:'#006D75',Stars:'#006847',Wild:'#154734',
+  // WNBA
   Aces:'#000000',Dream:'#C8102E',Fever:'#FFC72C',Liberty:'#6ECEB2',Lynx:'#236192',
   Mercury:'#CB6015',Mystics:'#002B5C',Sky:'#418FDE',Sparks:'#702F8A',Storm:'#2C5234',
   Sun:'#F05023',Wings:'#C4D600',
@@ -52,18 +57,20 @@ const TEAM_COLORS = {
   Georgia:'#BA0C2F','Georgia Tech':'#B3A369',Houston:'#C8102E',Illinois:'#E84A27',
   Indiana:'#990000',Iowa:'#FFCD00','Iowa St.':'#C8102E',Kansas:'#0051A5','Kansas St.':'#512888',
   Kentucky:'#0033A0',Louisville:'#AD0000',LSU:'#461D7C',Maryland:'#E03A3E',Miami:'#005030',
-  Michigan:'#00274C','Michigan St.':'#18453B',Minnesota:'#7A0019',Mississippi:'#CE1126',
-  'Mississippi St.':'#5D1F1A',Missouri:'#F1B82D',Nebraska:'#E41C38','North Carolina':'#7BAFD4',
-  'North Carolina St.':'#CC0000',Northwestern:'#4E2A84','Notre Dame':'#0C2340',
-  'Ohio State':'#BB0000',Oklahoma:'#841617','Oklahoma St.':'#FF7300','Ole Miss':'#CE1126',
-  Oregon:'#154733','Oregon St.':'#CC0000','Penn State':'#041E42',Pittsburgh:'#003594',
-  Purdue:'#CEB888',Rutgers:'#CC0033',SMU:'#0033A0','South Carolina':'#73000A',
-  Stanford:'#8C1515',Syracuse:'#D44500',TCU:'#4D1979',Tennessee:'#FF8200',Texas:'#BF5700',
-  'Texas A&M':'#500000','Texas Tech':'#CC0000',UCLA:'#2D68C4',USC:'#990000',Utah:'#CC0000',
-  Vanderbilt:'#866D4B',Virginia:'#232D4B','Virginia Tech':'#630031','Wake Forest':'#9E7E38',
-  Washington:'#4B2E83','Washington St.':'#981E32','West Virginia':'#002855',Wisconsin:'#C5050C',
+  Michigan:'#00274C','Michigan St.':'#18453B',Minnesota:'#7A0019','Mississippi St.':'#5D1F1A',
+  Missouri:'#F1B82D',Nebraska:'#E41C38','North Carolina':'#7BAFD4','North Carolina St.':'#CC0000',
+  Northwestern:'#4E2A84','Notre Dame':'#0C2340','Ohio State':'#BB0000',Oklahoma:'#841617',
+  'Oklahoma St.':'#FF7300','Ole Miss':'#CE1126',Oregon:'#154733','Oregon St.':'#CC0000',
+  'Penn State':'#041E42',Pittsburgh:'#003594',Purdue:'#CEB888',Rutgers:'#CC0033',
+  SMU:'#0033A0','South Carolina':'#73000A',Stanford:'#8C1515',Syracuse:'#D44500',
+  TCU:'#4D1979',Tennessee:'#FF8200',Texas:'#BF5700','Texas A&M':'#500000',
+  'Texas Tech':'#CC0000',UCLA:'#2D68C4',USC:'#990000',Utah:'#CC0000',Vanderbilt:'#866D4B',
+  Virginia:'#232D4B','Virginia Tech':'#630031','Wake Forest':'#9E7E38',
+  'Washington':'#4B2E83','Washington St.':'#981E32','West Virginia':'#002855',Wisconsin:'#C5050C',
   Connecticut:'#000E2F',Gonzaga:'#002469',Marquette:'#003366',Villanova:'#003366',
-  Xavier:'#002883','San Diego St.':'#C41230',Memphis:'#003087',
+  Xavier:'#002883','San Diego St.':'#C41230',Memphis:'#003087','NC State':'#CC0000',
+  'Arizona':'#CC0033','Arizona St.':'#8C1D40',Cincinnati:'#E00122',
+  // MLS
   'Atlanta United':'#80000A','Austin FC':'#00B140','Charlotte FC':'#1A85C8',
   'Chicago Fire':'#9A1B2E','Colorado Rapids':'#862633','Columbus Crew':'#FEDD00',
   'D.C. United':'#000000','FC Dallas':'#BF1B23','Inter Miami':'#F7B5CD','LA Galaxy':'#00245D',
@@ -121,9 +128,8 @@ export default function StepTeams({ leagues, selected, setSelected, onBack, onNe
       <div className={styles.teamGrid}>
         {teams.map(name => {
           const sel = isSelected(activeTab, name)
-          const color = TEAM_COLORS[name] || '#333'
-          const color = TEAM_COLORS[name] || '#5C0A10'
-const initials = name.split(' ').map(w => w[0]).join('').slice(0, 3).toUpperCase()
+          const color = TEAM_COLORS[name] || '#1a1a1a'
+          const initials = name.split(' ').map(w => w[0]).join('').slice(0, 3).toUpperCase()
           return (
             <div
               key={name}
